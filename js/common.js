62360156e59f5aea923af61081115e524e537d50
FahimShahriar202 <fahimshahriar202@gmail.com>
2024-10-14 20:01:41 +0600

    homepage done

--- js/common.js
+++ js/common.js
@@ -1,1 +1,5 @@
 // blog-btn  current-balance  
+// btn-donation btn-history 
+// noakhali-amount  btn-donate-now noakhali-input
+// feni-amount  feni-input  btn-donate-now-feni
+// quota-amount quota-input  btn-donate-now-quota
