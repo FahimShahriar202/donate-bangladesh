4ef7e220855bf655ee153ee50e0e4736462a61be
FahimShahriar202 <fahimshahriar202@gmail.com>
2024-10-15 01:50:25 +0600

    blog added

--- js/common.js
+++ js/common.js
@@ -3,4 +3,10 @@
 // btn-donation btn-history 
 // noakhali-amount  btn-donate-now noakhali-input
 // feni-amount  feni-input  btn-donate-now-feni
-// quota-amount quota-input  btn-donate-now-quota
+// quota-amount quota-input  btn-donate-now-
+
+
+document.getElementById('blog').addEventListener('click', function(event) {
+  event.preventDefault();
+  window.location.href = "/bl";
+});
